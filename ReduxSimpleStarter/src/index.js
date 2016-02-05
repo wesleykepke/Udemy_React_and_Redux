f8ae0023cbc0c17e53a725ad0b66f3c99ee0c31f
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-05 12:11:34 -0800

    Finished lecture #29

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'; // for rendering to the DOM
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyCb5C39mnDx1iGJQrCM2lS63OazpgCXlFQ";
 
 class App extends Component {
@@ -23,7 +24,8 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
