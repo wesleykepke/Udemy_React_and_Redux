5a03cb98a417dbc4cfce54e5035238a43e2c7fd5
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-05 16:01:22 -0800

    Finished lecture #41

--- Session4_ReactAndRedux/src/containers/book-list.js
+++ Session4_ReactAndRedux/src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
@@ -17,3 +18,23 @@ export default class BookList extends Component {
     );
   }
 }
+
+/*
+Purpose: Take app state as an argument, and whatever is returned
+from this function will show up as props inside of BookList.
+
+This function is what maps react to redux.
+
+Two important facts:
+If state changes, this function will immediately be called again
+and a re-rendering will take place.
+
+Whenever the state changes, it will be assigned as props above.
+*/
+function mapStateToProps(state) {
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
