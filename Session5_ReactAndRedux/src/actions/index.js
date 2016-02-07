9f300f0244d9ad0febf7e7810f87e330f037942d
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-07 09:37:40 -0800

    Finished lecture #56

--- Session5_ReactAndRedux/src/actions/index.js
+++ Session5_ReactAndRedux/src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY "0259d3b09670352110896c8d69958db1";
+import axios from 'axios';
+
+const API_KEY = "0259d3b09670352110896c8d69958db1";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// "city" is a string
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url); // returns a promise
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
