1f5cd9b7b73aeccd55b6dc45933d85da38991d9d
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-07 22:32:31 -0800

    Finished lecture #59

--- Session5_ReactAndRedux/src/reducers/reducer_weather.js
+++ Session5_ReactAndRedux/src/reducers/reducer_weather.js
@@ -1,3 +1,13 @@
-export default function(state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+/*
+Initial state is an array, because we want to store a list of
+weather stuff.
+*/
+export default function(state = [], action) {
+  switch(action.type) {
+    case FETCH_WEATHER:
+      return [ action.payload.data, ...state ]; // [city, city, city]
+  }
   return state;
 }
