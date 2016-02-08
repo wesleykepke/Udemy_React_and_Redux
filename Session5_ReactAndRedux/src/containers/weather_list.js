664fd0da6ad349eb6ff49f3f65c1639285234117
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-07 22:52:02 -0800

    Finished lecture #61

--- Session5_ReactAndRedux/src/containers/weather_list.js
+++ Session5_ReactAndRedux/src/containers/weather_list.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return(
       <table className="table table-hover">
@@ -14,7 +24,7 @@ class WeatherList extends Component {
           </tr>
         </thead>
         <tbody>
-
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
@@ -27,4 +37,5 @@ function mapStateToProps( {weather} ) {
   };
 }
 
+// Now have access to this.props.weather
 export default connect(mapStateToProps)(WeatherList);
