0b5f237cb0c231bf4752554f44c8297b1fbde767
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-09 23:23:05 -0800

    Finished lecture #72

--- Session6_ReactAndRedux/src/index.js
+++ Session6_ReactAndRedux/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
 
-import App from './components/app';
+/*
+History object: what part of the url react cares about
+*/
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} />
   </Provider>
   , document.querySelector('.container'));
