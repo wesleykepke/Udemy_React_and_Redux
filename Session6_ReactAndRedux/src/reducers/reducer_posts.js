9b5339248b755028cfd604338abdc778c1d49221
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-10 23:02:54 -0800

    Finished lecture #78

--- Session6_ReactAndRedux/src/reducers/reducer_posts.js
+++ Session6_ReactAndRedux/src/reducers/reducer_posts.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = { all: [], post: null };
 export default function(state = INITIAL_STATE, action) {
 	switch (action.type) {
 		case FETCH_POSTS:
-
+      return { ...state, all: action.payload.data };
 			break;
 
 		default:
