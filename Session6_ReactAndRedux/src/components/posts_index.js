4dc68588d51a6d10a259b342b8c1bcdc0fa11254
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-10 23:10:30 -0800

    Finished lecture #79

--- Session6_ReactAndRedux/src/components/posts_index.js
+++ Session6_ReactAndRedux/src/components/posts_index.js
@@ -1,4 +1,22 @@
-import React from 'react';
-export default () => {
-	return <div>List of blog posts.</div>; 
-};
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+	/*
+	This is a lifecycle method; React will call this function automatically
+	whenever a component is about to rendered to the DOM for the first time.
+
+	This essentially means that whenever out posts component is about the mount,
+	we should have the data fetched for us. 
+	*/
+	componentWillMount() {
+		console.log("This would be a good time to call an action creator to fetch posts");
+	}
+
+	render() {
+		return (
+			<div>List of blog posts</div>
+		);
+	}
+}
+
+export default PostsIndex;
