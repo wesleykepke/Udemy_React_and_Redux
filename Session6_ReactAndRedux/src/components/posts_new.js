6c8d7a761d9c4bf08e42c2ed33bb6b397b346298
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-11 22:38:14 -0800

    Finished lecture #85

--- Session6_ReactAndRedux/src/components/posts_new.js
+++ Session6_ReactAndRedux/src/components/posts_new.js
@@ -3,22 +3,31 @@ import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 	render() {
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		// equivalent to const handleSubmit = this.props.handleSubmit
+
+		/*
+		{...title} - this is destructuring. I want to pass this object into
+		input, but I just want all of the properties of the title object to
+		go into the input as keys:values
+		*/
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h3>Create A New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...title} />
 				</div>
 
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>
 
 				<div className="form-group">
 					<label>Content</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...content} />
 				</div>
 
 				<button type="submit" className="btn btn-primary">Submit</button>
@@ -27,6 +36,9 @@ class PostsNew extends Component {
 	}
 }
 
+/*
+reduxForms injects some helpers into this.props (like redux connect)
+*/
 export default reduxForm({
 	form: 'PostsNew', // needs to be a unique token
 	fields: ['title', 'categories', 'content']
