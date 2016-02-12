c7133483a78c2e617aca6b907711d9c977da0c2a
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-12 15:58:20 -0800

    Finished lecture #90

--- Session6_ReactAndRedux/src/components/posts_index.js
+++ Session6_ReactAndRedux/src/components/posts_index.js
@@ -15,6 +15,17 @@ class PostsIndex extends Component {
 		this.props.fetchPosts();
 	}
 
+	renderPosts() {
+		return this.props.posts.map((post) => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					<span className="pull-xs-right">{post.categories}</span>
+					<strong>{post.title}</strong>
+				</li>
+			)
+		})
+	}
+
 	render() {
 		return (
 			<div>
@@ -23,11 +34,20 @@ class PostsIndex extends Component {
 						Add a Post
 					</Link>
 				</div>
-				List of blog posts
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
+function mapStateToProps(state) {
+	return {
+		posts: state.posts.all
+	};
+}
+
 // First parameter is null because we have not defined mapStateToProps (yet)
-export default connect(null, {fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
